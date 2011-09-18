2849b16b05ee93afba71ad147da8e992390db640
Sawyer X <xsawyerx@cpan.org>
2011-09-18 18:47:50 +0300

    create files in generated directory

--- templates/create.sh
+++ templates/create.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+if [ -d templates ]; then
+    cd templates
+fi
+
 echo "Attempting to create index.html..."
-tpage index.tt > ../index.html
+tpage index.tt > ../generated/index.html
 if [ "$?" -ne "0" ]; then
 	echo "Unable to create index.html"
 	exit 1
 fi
 
 echo "Attempting to create previous.html..."
-tpage previous.tt > ../previous.html
+tpage previous.tt > ../generated/previous.html
 if [ "$?" -ne "0" ]; then
         echo "Unable to create previous.html"
         exit 1 
 fi 
 
 echo "Attempting to create previous.html..."
-tpage about.tt > ../about.html
+tpage about.tt > ../generated/about.html
 if [ "$?" -ne "0" ]; then
         echo "Unable to create about.html"
         exit 1 
