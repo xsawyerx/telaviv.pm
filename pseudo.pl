9738cfb23a8645b78fda1d1277dab61bbc5f3965
Sawyer X <xsawyerx@cpan.org>
2012-09-24 01:54:29 +0200

    rename under PM::TA namespace, document, add some required

--- pseudo.pl
+++ pseudo.pl
@@ -1,19 +1,19 @@
 use strict;
 use warnings;
 
-package Event;
+package PM::TA::Event;
 use Moo;
-has date  => ( is => 'ro' );
+has date  => ( is => 'ro', required => 1 );
 has intro => ( is => 'rw' );
 has talks => ( is => 'rw', default => sub {[]} );
 
-package Talk;
+package PM::TA::Talk;
 use Moo;
-has title    => ( is => 'ro' );
+has title    => ( is => 'ro', required => 1 );
 has speaker  => ( is => 'ro' );
 has abstract => ( is => 'rw' );
 
-package PseudoTA;
+package PM::TA::POD;
 use parent 'Pod::Simple';
 
 my ( $prev_type, $cur_type, $cur_event, $cur_talk );
@@ -42,6 +42,7 @@ sub _handle_text {
         push @{ $events{ $cur_event->date }->talks }, $cur_talk;
         $prev_type = $cur_type;
     } else {
+        print "  (unrecognized type $cur_type)\n";
         if ( $prev_type eq 'head1' ) {
             $cur_event->intro($text);
         } elsif ( $prev_type eq 'head2' ) {
@@ -52,9 +53,109 @@ sub _handle_text {
 }
 
 package main;
-my $pta = PseudoTA->new;
+my $pta = PM::TA::POD->new;
 $pta->parse_file('test.pod');
 
+my $event = $events{'June 27, 2012'};
+::p $event;
+__END__
 foreach my $event_date ( keys %events ) {
     ::p $events{$event_date}->talks;
 }
+
+__END__
+
+=head1 PM::TA::POD - Pod parser for TA.pm events
+
+=head1 SYNOPSIS
+
+    my $ptp = PM::TA::POD;
+    $ptp->parse_file('events.pod');
+
+    # use %events
+
+=head1 DESCRIPTION
+
+TA.pm had a lot of events, and meets regularly so these events grow. While it's
+not impossible to write HTML for all of them, there is a clear case for
+adopting a standard format, and making it easy to change it in the future.
+
+For that purpose, they were all writen in POD form (which is the most flexible
+format available) and this script parses that form in order to create a
+template, which will then be rendered to create a full static HTML file.
+
+=head1 WHY NOT <insert other format>
+
+Most other format (YAML, JSON, INI, XML) are serializing formats, and not meant
+for flexible amounts of texts with flexible structures while still making it
+easy for users to add new content. Yes, it's posisble with all those listed and
+more, but they clearly make it harder for users to keep adding.
+
+The POD format makes it extremely easy to add and remove and it's very simple
+and easy to parse.
+
+=head1 Structure
+
+    =head1 date
+
+    Possible description
+
+    =head2 Talk name :: Speaker
+
+    Possible description
+
+You may also include a list in any description which will still be part of the
+description and will be rendered as a list.
+
+    =over 4
+
+    =item * ITEM
+
+    =item * ITEM
+
+    =back
+
+=head1 Objects structure
+
+=head2 PM::TA::Event
+
+Event objects hold each event.
+
+=head3 Attributes
+
+=over 4
+
+=item * date
+
+Event date in the format of "February 29, 1984".
+
+=item * intro
+
+Introduction to the event.
+
+=item * talks
+
+An arrayref of talks of type PM::TA::Talk, described below.
+
+=back
+
+=head2 PM::TA::Talk
+
+=head3 Attributes
+
+=over 4
+
+=item * title
+
+Talk title.
+
+=item * speaker
+
+Talk speaker.
+
+=item * abstract
+
+Abstract for the talk.
+
+=back
+
